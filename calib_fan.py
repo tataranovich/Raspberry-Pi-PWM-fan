064854ef5af6b59037a5c469f07343bc51871ebb
Andrey Tataranovich <tataranovich@gmail.com>
2020-11-22 20:37:47 +0300

    Cleanup code and adopt for LibreELEC

--- calib_fan.py
+++ calib_fan.py
@@ -1,34 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import RPi.GPIO as GPIO
 import sys
-#import time
-#import os
+sys.path.append("/storage/.kodi/addons/virtual.rpi-tools/lib")
+import RPi.GPIO as GPIO
 
-FAN_PIN = 24
-WAIT_TIME = 1
-PWM_FREQ = 25
+FAN_PIN = 16
+PWM_FREQ = 10
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(FAN_PIN, GPIO.OUT, initial=GPIO.LOW)
-
 fan = GPIO.PWM(FAN_PIN, PWM_FREQ)
 fan.start(0)
-i = 0
-
-hyst = 1
-tempSteps = [50, 70]
-speedSteps = [0, 100]
-cpuTempOld = 0
 
 try:
     while 1:
         fanSpeed = float(input("Fan Speed: "))
         fan.ChangeDutyCycle(fanSpeed)
 
-
 except(KeyboardInterrupt):
-    print("Fan ctrl interrupted by keyboard")
+    print("Interrupted by keyboard")
     GPIO.cleanup()
     sys.exit()
